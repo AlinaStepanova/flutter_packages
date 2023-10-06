c66b7c490517bd2827e66e707f65637930428772
AlinaStepanova <stepanovaalina15@gmail.com>
2023-10-06 10:58:47 +0200

    update slackURL

--- packages/appinio_social_share/ios/Classes/ShareUtil.swift
+++ packages/appinio_social_share/ios/Classes/ShareUtil.swift
@@ -323,7 +323,7 @@ public class ShareUtil : NSObject, MFMailComposeViewControllerDelegate {
     func shareToSlack(args : [String: Any?],result: @escaping FlutterResult)  {
         let message = args[self.argMessage] as? String
 
-        let slackURL = "slack://user?text=\(message)"
+        let slackURL = "slack://app_redirect?text=\(message)"
 
         var characterSet = CharacterSet.urlQueryAllowed
         characterSet.insert(charactersIn: "?&")
